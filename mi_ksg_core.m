4ce8fb04fe02677db356bb064923ddd3f9e1da1d
lht0098 <lteng@ucdavis.edu>
2022-04-01 10:54:05 -0400

    added date of change to stab criteria

--- mi_ksg_core.m
+++ mi_ksg_core.m
@@ -536,6 +536,7 @@ classdef mi_ksg_core < handle
             stab_mat = triu(dataFrac_stab_matrix);
             
             % First, assess stability of each data frac comparison
+            %%% 20220331 LHT: stability_boolean = stab_mat < 1;
             stability_boolean = stab_mat < 1.5;
             
             % Determine if the frist four data fracs are stable
