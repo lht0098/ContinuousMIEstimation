215023c7524a0c2b5db0cb2649f5c3aac55caf00
lht0098 <lteng@ucdavis.edu>
2022-05-16 17:11:53 -0400

    updated mi_ksg_core >> find_k_value
    commented on changing from all ks to any ks

--- mi_ksg_core.m
+++ mi_ksg_core.m
@@ -361,6 +361,7 @@ classdef mi_ksg_core < handle
                     k_stab = get_stabMat_kvals(obj, ks(notBad_Ks));
                 end
                 % Get stability metric value for k stability
+                %%% changed all ks to any ks
                 if all(weighted_k < 1)
                     k_stability_weights = zeros(size(ks));
                 else
