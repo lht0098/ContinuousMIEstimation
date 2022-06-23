ba32fa3ef67ced72d6fefe116613057284e98018
neuRowsATL <bchung808@gmail.com>
2022-06-23 14:40:51 -0400

    Create run_behav_neur_analysis.m

---
+++ ExampleFileandScripts/Example File and Scripts/run_behav_neur_analysis.m
@@ -0,0 +1,28 @@
+%% =====  =====  =====  =====  ===== 
+% Run full analysis for: NN, NB, NNB
+%  =====  =====  =====  =====  ===== 
+
+standard_fileInfo = '20200422_bl21lb21_example_data';
+
+behav_fileInfo = strcat('example script/', datestr(date, 'mmddyyyy'));
+neural_fileInfo = strcat('example script/', datestr(date, 'mmddyyyy'));
+
+verbose_level = 5;
+
+%%
+% load('/Users/brycechung/Documents/GitHub/ContinuousMIEstimation/ExampleFileandScripts/Example File and Scripts/20200422_bl21lb21_example_data.mat', 'final_cycle_times');
+
+
+b = mi_data_pressure(behav_fileInfo, 'verbose', 4);
+
+b.set_data_files('ext', '.rhd');
+
+b.set_calibration('slope', 15.4467, 'first_file_mean', 1.5336, 'pressure_offset', 1.5336);
+
+b.build_behavior();
+
+% b.add_cycleTimes(final_cycle_times', strcat(standard_fileInfo, datestr(date, 'mmddyyyy')), 30000);
+
+b.process_behavior();
+
+
